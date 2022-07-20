5de3570f8143ffca6c0966def3cced416aafac70
Yash Kumar Singh <syash@ams-ubu-eng-019.vmware.com>
2022-07-20 14:51:39 +0200

    test

--- hack/verify/generate-grpc.sh
+++ hack/verify/generate-grpc.sh
@@ -17,7 +17,7 @@
 # Install the protobuf and protoc-gen for GO
 function install_grpc {
 
-    apt install -y protobuf-compiler
+    sudo apt install -y protobuf-compiler
 
     go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.28
     go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.2
